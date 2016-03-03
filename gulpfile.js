1657bed5acb1452aea24c2a26527c519523c1e54
Daniyal857 <daniyalkhan857@gmail.com>
2016-03-03 17:17:28 +0500

    minified our html file

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,10 @@ var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
-    connect = require('gulp-connect'),
+    minifyHTML = require('gulp-minify-html'),
     concat = require('gulp-concat');
 
 var env,
@@ -71,7 +72,7 @@ gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
-  gulp.watch(htmlSources, ['html']);
+  gulp.watch('builds/development/*html', ['html']);
   gulp.watch(jsonSources, ['json']);
 });
 
@@ -83,8 +84,10 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-  gulp.src(htmlSources)
-    .pipe(connect.reload())
+    gulp.src('builds/development/*.html')
+        .pipe(gulpif(env === 'production', minifyHTML()))
+        .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+        .pipe(connect.reload())
 });
 
 gulp.task('json', function() {
