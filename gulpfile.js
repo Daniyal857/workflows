1056790c419c67a90e2c12075a64606249d04961
Daniyal857 <daniyalkhan857@gmail.com>
2016-03-03 17:04:55 +0500

    Added gulp-if and minified production javascript

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
     connect = require('gulp-connect'),
     concat = require('gulp-concat');
 
@@ -48,6 +50,7 @@ gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
+    .pipe(gulpif(env === 'production', uglify()))
     .pipe(gulp.dest( outputDir + '/js'))
     .pipe(connect.reload())
 });
